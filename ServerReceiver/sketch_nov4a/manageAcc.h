aa48e4f63e8f659a76780f047cc1a47bf00273b8
SorteFatale <116297488+SorteFatale@users.noreply.github.com>
2024-11-16 18:49:44 +0100

    Add files via upload
    SERVER AGGIORNATO CON SOLO IL CODICE DELLO SCHERMO

---
+++ ServerReceiver/sketch_nov4a/manageAcc.h
@@ -0,0 +1,12 @@
+#ifndef manageAcc_h
+#define manageAcc_h
+
+#include <esp_now.h>
+
+
+int reducePlaces(esp_err_t result, int posti_liberi);
+int increasePlaces(esp_err_t result, int posti_liberi);
+
+
+
+#endif
