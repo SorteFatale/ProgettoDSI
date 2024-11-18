0a06a383a48d4018bd19b7d6104f5a2ce39e44fb
frabru99 <brunellofraancesco5@gmail.com>
2024-11-18 11:55:31 +0100

    Screen Code

--- ServerReceiver/sketch_nov4a/sketch_nov4a.ino
+++ ServerReceiver/sketch_nov4a/sketch_nov4a.ino
@@ -7,6 +7,7 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "./manageAcc.h"
+#include <LiquidCrystal_I2C.h>
 
 
 //------VARIABILI e STRUTTURE--------
@@ -39,9 +40,10 @@ struct_message board3;
 struct_message board4;
 
 //Matrice di indirizzi MAC, utile per inviare gli ACK ai rispettivi sender. E' una 4x6.
-uint8_t macMatrics[2][6] = {
+uint8_t macMatrics[3][6] = {
   {0xac, 0x15, 0x18, 0xe9, 0x8d, 0xf0},
-  {0xac, 0x15, 0x18, 0xe9, 0xb4, 0x24}
+  {0xac, 0x15, 0x18, 0xe9, 0xb4, 0x24},
+  {0xac, 0x15, 0x18, 0xe9, 0x9f, 0x4c}
 }; 
 
 
@@ -52,12 +54,41 @@ SemaphoreHandle_t xMutex;
 int posti_liberi = posti_totale;
 
 //Array di strutture di messaggio.
-struct_message boardsStruct[2] = {board1, board2}; //Saranno 4 board alla fine
+struct_message boardsStruct[2] = {board1, board2, board3}; //Saranno 4 board alla fine
 
+
+//oggetto lcd
+LiquidCrystal_I2C lcd(0x27, 16,2); 
 
 
 
 //------FUNZIONI------
+
+void init_lcd(){
+    // inizializzo lo schermo LCD
+  lcd.init();
+  // accendo la backlight del LCD                      
+  lcd.backlight();
+}
+
+
+//funzione per stampare il numero di posti attualmente disponibili
+void stampa_posti_schermo(LiquidCrystal_I2C lcd, int posti_liberi){
+  lcd.setCursor(0,0);
+  lcd.print("Posti liberi:");
+
+  lcd.setCursor(0,1);
+  lcd.print(posti_liberi);
+  lcd.print("  ");
+
+}
+
+//stampa che mostra la non disponibilità di posti
+void stampa_non_disp_schermo(LiquidCrystal_I2C lcd){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Posti finiti!");
+}
 
 
 void Task1(void *myData){ //Task da eseguire all'accesso alla risorsa condivisa, posti disponibili del parcheggio.
@@ -68,7 +99,7 @@ void Task1(void *myData){ //Task da eseguire all'accesso alla risorsa condivisa,
     Serial.println("Task sta accedendo alla risorsa condivisa.");
     delay(10);
 
-    checkMessagePosti(message, posti_liberi);    //facciamo check di ingresso o uscita
+    checkMessagePosti(message);    //facciamo check di ingresso o uscita
     
     xSemaphoreGive(xMutex); //Release
   }
@@ -78,61 +109,73 @@ void Task1(void *myData){ //Task da eseguire all'accesso alla risorsa condivisa,
 
 
 
-void checkMessagePosti(struct struct_message* message, int posti_liberi){
+void checkMessagePosti(struct struct_message* message){
 
     registrationPeer(message);
 
-    if(posti_liberi > 0){  //Se ci sono posti liberi...
+    if(message->x == 1){
+      //Se ho un messaggio di ingresso...
         
-        //Richiesta di ingresso del parcheggioi
-        if(message->x == 1){ //Il messaggio è uguale a 1?
+        //Se ci sono posti liberi...
+        if(posti_liberi > 0){
           
-          myAck.value=1; 
 
-          //Stampa di debug per mac address ricevuto
-          Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
-                  message->macSender[0], message->macSender[1], message->macSender[2],
-                  message->macSender[3], message->macSender[4], message->macSender[5]);
-          
-          //Invio ACK=1 al client per farlo entrare.
-          esp_err_t result = esp_now_send(macMatrics[message->id-1], (uint8_t *) &myAck, sizeof(myAck));
-        
+          esp_err_t result = openBar(message);
           //Aggiorno variabile posti_liberi
+
           posti_liberi = reducePlaces(result, posti_liberi); //riduciamo i posti
 
+
+          //stampo posti su lcd
+          if(posti_liberi!=0){
+            stampa_posti_schermo(lcd,posti_liberi);
+          } else {
+            stampa_non_disp_schermo(lcd);
+          }
           //funzione stampaPosti
           stampaPosti(result, &myAck, 0);
           
-        } 
-      
-      } else {
+        } else {
 
-        //Qui invece inviamo il messaggio di non disponibilità di posto
-        myAck.value=0;
-        esp_err_t result = esp_now_send(macMatrics[message->id-1], (uint8_t *) &myAck, sizeof(myAck));
-        stampaPosti(result, &myAck, 0);
+            notOpenBar(message);
+        }
 
-      }
-
-
-      //Richiesta di uscita dal parcheggio
-      if(message->x == 0 && posti_liberi < posti_totale ){
-         //ACK per alzata sbarra di uscita
-         myAck.value=1;
-
-        //Stampa di debug per mac address ricevuto
-        Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
-                message->macSender[0], message->macSender[1], message->macSender[2],
-                message->macSender[3], message->macSender[4], message->macSender[5]);
-
-
-         esp_err_t result = esp_now_send(macMatrics[message->id-1], (uint8_t *) &myAck, sizeof(myAck));
-         posti_liberi = increasePlaces(result, posti_liberi);
-         stampaPosti(result, &myAck, 1);
-      } 
+    //Richiesta di uscita dal parcheggio
+    } else if(message->x == 0 && posti_liberi < posti_totale ){
+        //ACK per alzata sbarra di uscita
+        esp_err_t result = openBar(message);
+        posti_liberi = increasePlaces(result, posti_liberi);
+        stampa_posti_schermo(lcd,posti_liberi);
+        stampaPosti(result, &myAck, 1);
+    } 
 
 }
 
+
+esp_err_t openBar(struct struct_message* message){
+
+    myAck.value=1; 
+
+    //Stampa di debug per mac address ricevuto
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+            message->macSender[0], message->macSender[1], message->macSender[2],
+            message->macSender[3], message->macSender[4], message->macSender[5]);
+          
+    //Invio ACK=1 al client per farlo entrare.
+    esp_err_t result = esp_now_send(macMatrics[message->id-1], (uint8_t *) &myAck, sizeof(myAck));
+
+    return result;
+}
+
+
+esp_err_t notOpenBar(struct struct_message* message){
+  //Qui invece inviamo il messaggio di non disponibilità di posto
+    myAck.value=0;
+    esp_err_t result = esp_now_send(macMatrics[message->id-1], (uint8_t *) &myAck, sizeof(myAck));
+    stampaPosti(result, &myAck, 0);
+
+    return result;
+} 
 
 
 //Questa è una funzione che stampa l'avvenuto invio del messaggio di Ack. 
@@ -154,43 +197,18 @@ void stampaPosti(esp_err_t result, struct ack_structure* myAck, int sel){
 }
 
 
-/* Si potrebbero eliminare
-void reducePlaces(esp_err_t result, int* posti_liberi){
 
-  if (result == ESP_OK) {
-      Serial.println("Ack=1 sent with success");
-      Serial.println("Decremento il numero di posti!");
-      posti_liberi = posti_liberi - 1; 
-      Serial.println(posti_liberi);
-    }
-    else {
-      Serial.println("Error sending the data");
-    }
-
-}
-
-
-void increasePlaces(esp_err_t result, int posti_liberi){
-  if(result==ESP_OK){
-   Serial.println("Incremento il numero di posti"); //Incremento posti per uscire
-   posti_liberi = posti_liberi + 1; 
-   Serial.println(posti_liberi);
-
-  } else {
-    Serial.println("Error sending the data");
-
-  }
-
-}
-*/
 
 //Registrazione al Peer se non è stato già fatto.
 void registrationPeer(struct struct_message* message){
       // Aggiungi il peer se non esiste già
+
+      memcpy(peerInfo.peer_addr, macMatrics[message->id-1], 6);
+      peerInfo.channel = 0;
+      peerInfo.encrypt = false;
       if (!esp_now_is_peer_exist(message->macSender)) {
-          memcpy(peerInfo.peer_addr, macMatrics[message->id-1], 6);
-          peerInfo.channel = 0;
-          peerInfo.encrypt = false;
+          
+          
           if (esp_now_add_peer(&peerInfo) != ESP_OK) {
               Serial.println("Errore nell'aggiungere il peer");
               return;
@@ -221,13 +239,11 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) 
     Serial.println("ID non valido, pacchetto ignorato.");
   }
 
-  xTaskCreate(Task1, "Task1", 1000, &myData, 1, NULL); //Creazione del Task 
+  xTaskCreate(Task1, "Task1", 10000, &myData, 1, NULL); //Creazione del Task 
 
 
   Serial.println();
 }
-
-
 
 
 void setup() {
@@ -242,6 +258,10 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.STA.begin();
 
+  //Inizializzazione schermo 
+  init_lcd();
+  stampa_posti_schermo(lcd,posti_liberi);
+
 
   //Inizializzazione ESP-Now
   if (esp_now_init() != ESP_OK) {
@@ -250,7 +270,6 @@ void setup() {
   }
 
   //Inizializzazione schermo 
-  
   
   //Ci mettiamo in ascolto
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv)); 
